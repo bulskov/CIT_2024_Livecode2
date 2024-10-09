f5b752dfc823d5f9d620d48a7a3e27a296e009a1
bulskov <bulskov@ruc.dk>
2024-10-09 11:06:28 +0200

    create client and connect to server

--- Client/ClientProgram.cs
+++ Client/ClientProgram.cs
@@ -1,1 +1,11 @@
 ﻿
+using System.Net;
+using System.Net.Sockets;
+
+var port = 5000;
+
+var client = new TcpClient();
+
+client.Connect(IPAddress.Loopback, port);
+
+Console.WriteLine("Client connected!!!");
