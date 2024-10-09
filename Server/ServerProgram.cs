e5483784c97b74264f2c96ae6faf408e6035fdb9
bulskov <bulskov@ruc.dk>
2024-10-09 11:01:12 +0200

    first part of the server

--- Server/ServerProgram.cs
+++ Server/ServerProgram.cs
@@ -1,1 +1,16 @@
 ﻿
+using System.Net;
+using System.Net.Sockets;
+
+var port = 5000;
+
+var server = new TcpListener(IPAddress.Loopback, port); // IPv4 127.0.0.1 IPv6 ::1
+server.Start();
+
+Console.WriteLine($"Server started on port {port}");
+
+while(true)
+{
+    var client = server.AcceptTcpClient();
+    Console.WriteLine("Client connected!!!");
+}
