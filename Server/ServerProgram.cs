d58c7d78cc0356a6a36752f149d5ba33534d5c7a
Henrik Bulskov <bulskov@ruc.dk>
2024-10-09 15:56:54 +0200

    Refactor server

--- Server/ServerProgram.cs
+++ Server/ServerProgram.cs
@@ -1,37 +1,9 @@
-﻿
-using System.Net;
+﻿using System.Net;
 using System.Net.Sockets;
 using System.Text;
 
-var port = 5000;
+var server = new Server(5000);
 
-var server = new TcpListener(IPAddress.Loopback, port); // IPv4 127.0.0.1 IPv6 ::1
-server.Start();
+server.Run();
 
-Console.WriteLine($"Server started on port {port}");
 
-while(true)
-{
-    var client = server.AcceptTcpClient();
-    Console.WriteLine("Client connected!!!");
-
-    try
-    {
-        var stream = client.GetStream();
-
-        var buffer = new byte[1024];
-
-        stream.Read(buffer);
-
-        var msg = Encoding.UTF8.GetString(buffer);
-
-        Console.WriteLine("Message from client: " + msg);
-
-        buffer = Encoding.UTF8.GetBytes(msg.ToUpper());
-        stream.Write(buffer);
-    }
-    catch { }
-
-   
-
-}
